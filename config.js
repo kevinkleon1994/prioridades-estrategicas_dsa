9c7da8dbaef86e32e65e7fef06eb1d581d6cab12
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-11 09:57:40 -0300

    Update version from 1.9.0 to 1.10.0

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ window.APP_CONFIG = Object.freeze({
   FIELD: "Missão Oeste do Pará",
   UNION: "União Norte Brasileira",
   DIVISION: "Divisão Sul-Americana",
-  VERSION: "1.9.0",
+  VERSION: "1.10.0",
   API_VERSION: "1.8"
 });
