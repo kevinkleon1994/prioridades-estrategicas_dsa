787bfe3643e96a910239c52264ba58ef79eb6ea3
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-11 10:09:03 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.APP_CONFIG = Object.freeze({
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzYHlBlPviDKXMwslym0FLh5HMdoWm2UgNW8OzQl2tnsk4WsT8ifZw1TiVll5XLaKSr5Q/exec",
+  FIELD: "Missão Oeste do Pará",
+  UNION: "União Norte Brasileira",
+  DIVISION: "Divisão Sul-Americana",
+  VERSION: "1.10.1",
+  API_VERSION: "1.8",
+  TRANSPORT: "bridge"
+});
