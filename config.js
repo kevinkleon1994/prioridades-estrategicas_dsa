ec58cf516cd721bf1fab4ba26f4f9582c5c63a53
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-11 18:45:01 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ window.APP_CONFIG = Object.freeze({
   FIELD: "Missão Oeste do Pará",
   UNION: "União Norte Brasileira",
   DIVISION: "Divisão Sul-Americana",
-  VERSION: "1.11.2",
+  VERSION: "1.11.3",
   API_VERSION: "1.8"
 });
