30b0f5b04f1df5302fbb2102b1229464df51c054
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-11 11:42:27 -0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-window.APP_CONFIG = Object.freeze({
-  API_PROXY_URL: "https://prioridades-dsa-api.kevinkleon1994.workers.dev",
-  FIELD: "Missão Oeste do Pará",
-  UNION: "União Norte Brasileira",
-  DIVISION: "Divisão Sul-Americana",
-  VERSION: "1.10.4",
-  API_VERSION: "1.8",
-  TRANSPORT: "cloudflare-proxy"
-});
