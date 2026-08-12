922bb48f0146cc6964c67afaebd035fd76ae928c
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-11 21:16:23 -0300

    Update version from 1.11.5 to 1.11.6

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ window.APP_CONFIG = Object.freeze({
   FIELD: "Missão Oeste do Pará",
   UNION: "União Norte Brasileira",
   DIVISION: "Divisão Sul-Americana",
-  VERSION: "1.11.5",
+  VERSION: "1.11.6",
   API_VERSION: "1.8"
 });
